6f07831dc877d16b894065806c935a8d73017377
贺晓飞 <hexiaofei@iscs.com.cn>
2017-03-22 17:53:14 +0800

    again 提交.

--- app/src/main/java/gank/minifly/com/gankgirl/activity_project/VideoGankViewDetailActivity.java
+++ app/src/main/java/gank/minifly/com/gankgirl/activity_project/VideoGankViewDetailActivity.java
@@ -1,13 +1,14 @@
 package gank.minifly.com.gankgirl.activity_project;
 
 import android.os.Bundle;
+import android.view.View;
 
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 import gank.minifly.com.gankgirl.R;
 import gank.minifly.com.gankgirl.common.imageloader.ImageloaderEngin;
 import gank.minifly.com.gankgirl.common.imageloader.ImageloaderPoxyImp;
 
-public class VideoGankViewDetailActivity extends BaseActivity {
+public class VideoGankViewDetailActivity extends BaseActivity implements View.OnClickListener{
     private JCVideoPlayerStandard mJcVideoPlayerStandard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +18,26 @@ public class VideoGankViewDetailActivity extends BaseActivity {
     }
 
     public void initView(){
+        Bundle bundle  = getIntent().getExtras();
+
+        if(bundle!=null && bundle.containsKey("")){
+
+        }
 
         mJcVideoPlayerStandard = (JCVideoPlayerStandard) findViewById(R.id.gank_video_jc_video);
         mJcVideoPlayerStandard.setUp("http://video.jiecao.fm/11/23/xin/%E5%81%87%E4%BA%BA.mp4",JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "嫂子不信");
         ImageloaderPoxyImp.getInstance().setLoader(new ImageloaderEngin(mContext)).displayImage("http://img4.jiecaojingxuan.com/2016/11/23/00b026e7-b830-4994-bc87-38f4033806a6.jpg@!640_360",mJcVideoPlayerStandard.thumbImageView);
 
+        mJcVideoPlayerStandard.setOnClickListener(this);
+
     }
 
-
-    //JCVideoPlayerStandard.startFullscreen(this, JCVideoPlayerStandard.class, "http://2449.vod.myqcloud.com/2449_22ca37a6ea9011e5acaaf51d105342e3.f20.mp4", "嫂子辛苦了");
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.gank_video_jc_video:
+                JCVideoPlayerStandard.startFullscreen(this, JCVideoPlayerStandard.class, "http://video.jiecao.fm/11/23/xin/%E5%81%87%E4%BA%BA.mp4", "嫂子辛苦了");
+                break;
+        }
+    }
 }
--- app/src/main/java/gank/minifly/com/gankgirl/fragment_project/MainOtherFragment.java
+++ app/src/main/java/gank/minifly/com/gankgirl/fragment_project/MainOtherFragment.java
@@ -3,29 +3,13 @@ package gank.minifly.com.gankgirl.fragment_project;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.alibaba.fastjson.JSON;
-import com.yolanda.nohttp.NoHttp;
-import com.yolanda.nohttp.rest.OnResponseListener;
-import com.yolanda.nohttp.rest.Request;
-import com.yolanda.nohttp.rest.Response;
-
-import java.util.List;
-
 import gank.minifly.com.gankgirl.R;
-import gank.minifly.com.gankgirl.adapter.PhotoAdapter;
-import gank.minifly.com.gankgirl.bean.FuliRequestBean;
-import gank.minifly.com.gankgirl.bean.FuliResponseBean;
-import gank.minifly.com.gankgirl.common.http.OnLoadListener;
-import gank.minifly.com.gankgirl.common.http.http_oo.NohttpEngin;
-import gank.minifly.com.gankgirl.common.tools.LogUtils;
-import gank.minifly.com.gankgirl.constant.UrlConstant;
+import gank.minifly.com.gankgirl.activity_project.VideoGankViewDetailActivity;
 
 /**
  * author ：minifly
@@ -33,91 +17,32 @@ import gank.minifly.com.gankgirl.constant.UrlConstant;
  * time: 20:24
  * desc:
  */
-public class MainOtherFragment extends BaseFragment {
+public class MainOtherFragment extends BaseFragment implements View.OnClickListener{
 
     private View view;
     private Context mContext;
-    private RecyclerView photoFragmentRecyclerview;
-    private SwipeRefreshLayout mySwipeRefreshLayout;
-    private List<FuliResponseBean.ResultsBean> currentPhotoList;
+    private FloatingActionButton floatingActionButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_main_photolist, container, false);
+        view = inflater.inflate(R.layout.fragment_main_other, container, false);
         initView();
         return view;
     }
 
     public void initView() {
         mContext = getContext();
-
-        photoFragmentRecyclerview = (RecyclerView) view.findViewById(R.id.photo_fragment_recyclerview);
-        mySwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.photo_fragment_swipe_refresh_layout);
-
-
+        floatingActionButton = (FloatingActionButton) view.findViewById(R.id.main_activity_other_up_arrow_but);
+        floatingActionButton.setOnClickListener(this);
     }
 
-
-    public void request(String pageNum) {
-
-        String url = UrlConstant.FULI_URL + "/" + pageNum;// /1来取第几页
-
-        FuliRequestBean requestData = new FuliRequestBean();
-        requestData.setRequestUrl(url);
-
-        Request<String> request = NohttpEngin.getRequest(requestData,NohttpEngin.METHOD_GET);
-
-        requestQueue = NoHttp.newRequestQueue();
-        requestQueue.add(1, request, onResponseListener);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.main_activity_other_up_arrow_but:
+                startActivity(VideoGankViewDetailActivity.class,null);
+                break;
+        }
     }
-
-
-    OnResponseListener<String> onResponseListener = new OnLoadListener<String>() {
-
-        @Override
-        public void onSuccess(int what, Response<String> response) {
-            LogUtils.showErrLog("" + response.toString());
-            switch (what) {
-
-                case 1:
-                    try {
-                        FuliResponseBean resoureseBean = JSON.parseObject(response.get(), FuliResponseBean.class);
-                        if(!resoureseBean.isError()){
-                            currentPhotoList.addAll(resoureseBean.getResults());
-                            photoFragmentRecyclerview.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
-                            //设置item之间的间隔
-                            photoFragmentRecyclerview.setAdapter(new PhotoAdapter(mContext, currentPhotoList));
-
-                        }else{
-                            showToast("查询失败");
-                        }
-                    } catch (Exception e) {
-                        showToast("数据解析异常");
-                    }
-                    break;
-
-            }
-
-
-        }
-
-        @Override
-        public void onError(int what, Response<String> response) {
-
-        }
-
-        @Override
-        public void onFinish(int what) {
-            super.onFinish(what);
-            hideProgressDialog();
-        }
-
-        @Override
-        public void onStart(int what) {
-            super.onStart(what);
-            showProgressDialog();
-        }
-    };
-
 }
